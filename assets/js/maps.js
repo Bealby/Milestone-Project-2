d2338d005a0dc21755f752c5cae35a1eb952d3c3
Bealby <mgbealby@gmail.com>
2020-04-23 10:53:06 +0000

    Setting lat and long to google maps

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,9 +1,11 @@
-
-function initMap() {
-    var map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 3,
-        center: {
-            lat: 46.619261,
-            lng: -33.134766
+    function initMap() {
+            let map = new google.maps.Map(document.getElementById("map"), {
+                zoom: 5.6,
+                center: {
+                    lat: 53.480759,
+                    lng: -2.24263
+                }
+            });
         }
-    });
+        
+        
