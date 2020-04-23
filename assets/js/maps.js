2329ae575ace519ac3a25fdeb27b55ac1752ad72
Bealby <mgbealby@gmail.com>
2020-04-23 11:41:23 +0000

    Added lat and long for google maps

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -6,6 +6,28 @@
                     lng: -3.119770,
                 }
             });
-        }
+
+            let labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+            let markers = locations.map(function(location, i) {
+          return new google.maps.Marker({
+            position: location,
+            label: labels[i % labels.length]
+          });
+        });
+
+        let markerCluster = new MarkerClusterer(map, markers,
+            {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
+      }
+
+       let locations = [
+        {lat: 56.2082078, lng: -3.1495175},
+         {lat: 55.864239, lng: -4.251806},
+          {lat: 51.431443, lng: -2.189674},
+           {lat: 54.2887008, lng: -1.5933179},
+        ]
+
+        
+            
         
         
