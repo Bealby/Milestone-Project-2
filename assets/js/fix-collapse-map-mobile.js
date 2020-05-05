ceea7a37e5acba81fbec3db00a3e153a14bfc4be
Bealby <mgbealby@gmail.com>
2020-05-05 18:37:04 +0000

    Added comment to javascript

--- assets/js/fix-collapse-map-mobile.js
+++ assets/js/fix-collapse-map-mobile.js
@@ -1,3 +1,5 @@
+// On mobile devices the navbar did not collaspe when links were clicked. The jQuery code below fixed this issue.
+
 $(document).ready(function() {
     $(".navbar-nav li a").click(function(event) {
         $(".navbar-collapse").collapse('hide');
